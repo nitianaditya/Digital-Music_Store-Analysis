5d79fa00dec9f6cb56f9dd5a70f50c04c46c5169
Aditya Singh <112848047+nitianaditya@users.noreply.github.com>
2023-10-10 07:44:10 +0530

    Update Digital Music Store Analysis.sql

--- Digital Music Store Analysis.sql
+++ Digital Music Store Analysis.sql
@@ -45,8 +45,8 @@ LIMIT 1;
 SELECT DISTINCT email AS Email,first_name AS FirstName, last_name AS LastName, genre.name AS Name
 FROM customer
 JOIN invoice ON invoice.customer_id = customer.customer_id
-JOIN invoiceline ON invoiceline.invoice_id = invoice.invoice_id
-JOIN track ON track.track_id = invoiceline.track_id
+JOIN invoice_line ON invoice_line.invoice_id = invoice.invoice_id
+JOIN track ON track.track_id = invoice_line.track_id
 JOIN genre ON genre.genre_id = track.genre_id
 WHERE genre.name LIKE 'Rock'
 ORDER BY email;
